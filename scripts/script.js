640d0bb5be540b6ff0631e59bc67cdb5f73c06f3
alireza bisout <alirezabisout1145@gmail.com>
2024-01-25 15:00:18 +0330

    Completing the portfolio slider
    It is running out, it needs small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+var swiper = new Swiper(".mySwiperOne", {
+    slidesPerView: 3,
     spaceBetween: 0,
     loop: true,
     autoplay: {
@@ -7,5 +7,51 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
     speed: 4500,
-    allowTouchMove: false
+    allowTouchMove: false,
+    breakpoints: {
+        // در سایز تبلت، نمایش 2 محتوا
+        768: {
+          slidesPerView: 4,
+        },
+        // در سایز موبایل، نمایش 1 محتوا
+        480: {
+          slidesPerView: 3,
+        },
+        // در سایز دسکتاپ، نمایش 3 محتوا
+        1200: {
+          slidesPerView: 4,
+        },
+      },
 });
+
+var swiper = new Swiper(".mySwiperTwo", {
+
+    slidesPerView: "auto",
+    grabCursor: true,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+
+    },
+    autoplay: {
+      delay: 2800,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      // در سایز تبلت، نمایش 2 محتوا
+      768: {
+        slidesPerView: 2,
+      },
+      // در سایز موبایل، نمایش 1 محتوا
+      480: {
+        slidesPerView: 1,
+      },
+      // در سایز دسکتاپ، نمایش 3 محتوا
+      1200: {
+        slidesPerView: 3,
+      },
+    },
+
+});
+

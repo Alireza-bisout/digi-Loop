511f9c14c06e17f556e19dedec0b138dddccd6ff
alireza bisout <alirezabisout1145@gmail.com>
2024-01-25 12:38:57 +0330

    The first changes
    It just starts here

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    speed: 4500,
+    allowTouchMove: false
+});
